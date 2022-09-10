178d6adafda5b6b440d9fec1b80f11e02b6df76b
eugenesteph <105042905+eugenesteph@users.noreply.github.com>
2022-09-10 19:13:24 +0300

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,6 @@
+#this is a game in python for guessing words 
+adj=input('enter an adjective')
+name=input('your name? :')
+activity=input('what do you love doing? :')
+madlib=f"Hello my name is {name}.I love {activity},because its so {adj}."
+print(madlib)
